57cb25b5cc3e24ee607f8981477151c2fd448164
Mohammed Abdullah <mohammed567abdullah@gmail.com>
2025-11-10 21:34:10 +0600

    Delete python_phitron directory

--- python_phitron/built in functions/str.find().py
+++
@@ -1,6 +0,0 @@
-string = "welcome to my house"
-
-print("welcome" in string)
-
-print(string.find("m"))
-print(string.rfind("m"))
--- python_phitron/procedural/p0 - Practice.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-
-student1 = Student("John")
-print(student1.name)
